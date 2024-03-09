70a385d4a0c4d59b757fb83d014e9cef1f4c8b19
KIIT <amananoopsaxena10@gmail.com>
2024-03-10 02:19:30 +0530

    Unfinished project

---
+++ app/src/main/java/com/aman/chatdevelopment/listeners/UserListener.java
@@ -0,0 +1,8 @@
+package com.aman.chatdevelopment.listeners;
+
+import com.aman.chatdevelopment.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+
+}
---
+++ app/src/main/java/com/aman/chatdevelopment/models/ChatMessage.java
@@ -0,0 +1,10 @@
+package com.aman.chatdevelopment.models;
+
+import java.util.Date;
+
+public class ChatMessage {
+    public String senderId,receiverId,message,dataTime;
+    public Date dateObject;
+
+
+}
